fdafd8a1bb64fd37bb4afcf2024bc74ccb26c64a
jakmarmaani <mohamed.a.elmonim@gmail.com>
2026-09-04 15:15:58 +0100

    Bump BrainQuest touch adventure cache to v7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='brainquest-kids-v6';
-const ASSETS=['./','./index.html','./styles.css?v=6','./mobile-fix.css?v=6','./game.js?v=6','./manifest.webmanifest'];
+const CACHE='brainquest-kids-v7';
+const ASSETS=['./','./index.html','./styles.css?v=7','./mobile-fix.css?v=6','./game.js?v=7','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))})
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))})
